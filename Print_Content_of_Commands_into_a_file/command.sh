e6aca7d93022b6e1149a3f1e45fe6c22feff8d75
obinna <obynodavid12@gmail.com>
2024-04-28 12:19:50 -0400

    pushing files

---
+++ Print_Content_of_Commands_into_a_file/command.sh
@@ -0,0 +1,19 @@
+ 
+#!/bin/bash
+
+get_system_info() {
+    echo "------ Output into check.txt ------"
+	whoami
+    echo "------ Output into check.txt ------"
+    hostname
+    echo "------ Output into check.txt ------"
+	date
+}
+
+echo "output into check.txt"
+get_system_info > check.txt
+
+# ( whoami; hostname; date; ) >check.txt
+
+# { whoami; hostname; date; } >check.txt
+
