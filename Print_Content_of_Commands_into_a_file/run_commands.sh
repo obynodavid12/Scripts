65b267a77b47268be745721a7f12c54e7628b705
obinna <obynodavid12@gmail.com>
2024-04-30 22:22:26 -0400

    updating run_command

--- Print_Content_of_Commands_into_a_file/run_commands.sh
+++ Print_Content_of_Commands_into_a_file/run_commands.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Run multiple commands
-whoami; echo '---------';sleep 1;
-hostname; echo '---------';sleep 1;
-ls -l; echo '---------';sleep 1;
-date; echo '---------';sleep 1;
+whoami > file.txt; echo '--------- name';sleep 1;
+hostname >> file.txt; echo '--------- hostname';sleep 1;
+ls -l >> file.txt; echo '--------- listing files';sleep 1;
+date >> file.txt; echo '--------- show date';sleep 1;
 
-# Redirect the output to file.txt
-whoami > file.txt
-hostname >> file.txt
-ls -l >> file.txt
-date >> file.txt
+# # Redirect the output to file.txt
+# whoami > file.txt
+# hostname >> file.txt
+# ls -l >> file.txt
+# date >> file.txt
